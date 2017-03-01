fd62624cb9668211806f7b88403f8faa5ff2344b
5urge <Surge@Sergiis-MacBook-Air.local>
2017-02-28 22:31:03 -0800

    asda

--- mySolutions/assignment3trial/app.js
+++ mySolutions/assignment3trial/app.js
@@ -17,7 +17,7 @@ function ShoppingListDirective() {
     },
     // controller: 'ShoppingListDirectiveController as list',
     controller: ShoppingListDirectiveController,
-    controllerAs: 'list',
+    controllerAs: 'listpoop',
     bindToController: true
   };
 
