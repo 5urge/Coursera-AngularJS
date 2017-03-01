16c227626a0a4f3cfc0b1014781c4a2b28596e9d
5urge <Surge@Sergiis-MacBook-Air.local>
2017-02-28 22:33:53 -0800

    asdas

--- mySolutions/assignment3trial/app.js
+++ mySolutions/assignment3trial/app.js
@@ -14,6 +14,7 @@ function ShoppingListDirective() {
     scope: {
       items: '<',
       title: '@'
+      
     },
     // controller: 'ShoppingListDirectiveController as list',
     controller: ShoppingListDirectiveController,
